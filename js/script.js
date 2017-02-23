256a18075d5eb9a33e0bdfde55888f664295c7aa
Yogender <ybisht1997@gmail.com>
2017-02-23 23:05:25 +0530

    Enhancements
    web sage fonts added
    margin and padding removed from all elements
    border box property added
    navbar content centered horizontally
    jumbotron height set to 50 % of viewport
    main content and interestsSection content now looks merged

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ function scrollAnimate() {
 	$("nav a").click(function(event) {
 		event.preventDefault();
 		var hash = this.hash;
-		$("html,body").animate({scrollTop: $(hash).offset().top-10},1000);
+		$("html,body").animate({scrollTop: $(hash).offset().top-20},1000);
 	});
 }
